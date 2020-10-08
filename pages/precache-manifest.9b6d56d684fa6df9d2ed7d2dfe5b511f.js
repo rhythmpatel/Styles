6f919f1445acc830d4ea5e1dfc8f5c875d593612
JC <jashwant.chouhan@gmail.com>
2020-10-08 23:46:30 +0530

    Updates

---
+++ pages/precache-manifest.9b6d56d684fa6df9d2ed7d2dfe5b511f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a862cdaa82ce231f169ad2ac2aa4b3",
+    "url": "/pages/index.html"
+  },
+  {
+    "revision": "72840f31205c8e110fca",
+    "url": "/pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c57cc330fa6696ff2f0",
+    "url": "/pages/static/js/2.634a969a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pages/static/js/2.634a969a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72840f31205c8e110fca",
+    "url": "/pages/static/js/main.6e517d7c.chunk.js"
+  },
+  {
+    "revision": "9c71bd05fbcbdb312690",
+    "url": "/pages/static/js/runtime-main.d678a7f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
