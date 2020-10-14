f2c82479ed3d8a36a7eb6674e10a56b6e99c914b
JC <jashwant.chouhan@gmail.com>
2020-10-14 13:32:41 +0530

    updated for styles layout 14-10-2020 13-32

---
+++ pages/src/routes/index.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import useWindowSize from '../hooks/useWindowSize';
+import PrivateSection from './PrivateSection';
+//import PublicRoutes from './PublicRoutes';
+
+function Routes() {
+    const { pathname } = useLocation();
+    // eslint-disable-next-line no-unused-vars
+    const [width, height] = useWindowSize();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+
+    return <PrivateSection />;
+}
+
+export default Routes;
