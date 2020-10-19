9c6730b048666485b2480113b21f371593e34cc5
Rhythm Ruparelia <rhythm.ruparelia@noovosoft.com>
2020-10-19 21:25:10 +0530

    Updates

---
+++ precache-manifest.d1fb4c4bb52187104806b032435f1f73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f221f20145a03306b74f9177392b68f",
+    "url": "/styles/index.html"
+  },
+  {
+    "revision": "7edeb3fdff07decc6968",
+    "url": "/styles/static/css/main.f4b5660e.chunk.css"
+  },
+  {
+    "revision": "62d375bc3e1eaaeaf986",
+    "url": "/styles/static/js/2.1a03a710.chunk.js"
+  },
+  {
+    "revision": "1e7a7346baa31f7b98172df30d46cdba",
+    "url": "/styles/static/js/2.1a03a710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc008d8cdfd1f1382ce7",
+    "url": "/styles/static/js/3.56c15239.chunk.js"
+  },
+  {
+    "revision": "7edeb3fdff07decc6968",
+    "url": "/styles/static/js/main.154d3757.chunk.js"
+  },
+  {
+    "revision": "4d355e2b72c7b4a528d8",
+    "url": "/styles/static/js/runtime-main.763a5599.js"
+  },
+  {
+    "revision": "e86c62802179e40f47b1e940c06944f0",
+    "url": "/styles/static/media/logo.e86c6280.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/iservice/precache-manifest.84c6ccfe7143fdb57de9e0f161af5100.js"
+  "/styles/precache-manifest.d1fb4c4bb52187104806b032435f1f73.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iservice/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/styles/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
