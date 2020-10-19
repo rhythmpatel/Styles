8cdd953a2ef47ecded0968c613168a06a1549463
Rhythm Ruparelia <rhythmpatel@users.noreply.github.com>
2020-10-19 14:49:51 +0000

    Updates

---
+++ precache-manifest.f1a4064f0e1bfe98464a7993b99e2a1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92fc4e579a9a86e8b09ebe84b172ec2",
+    "url": "/pages/index.html"
+  },
+  {
+    "revision": "64f0daed439072f42e91",
+    "url": "/pages/static/css/main.7d927f25.chunk.css"
+  },
+  {
+    "revision": "62d375bc3e1eaaeaf986",
+    "url": "/pages/static/js/2.1a03a710.chunk.js"
+  },
+  {
+    "revision": "1e7a7346baa31f7b98172df30d46cdba",
+    "url": "/pages/static/js/2.1a03a710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc008d8cdfd1f1382ce7",
+    "url": "/pages/static/js/3.56c15239.chunk.js"
+  },
+  {
+    "revision": "64f0daed439072f42e91",
+    "url": "/pages/static/js/main.e7292c68.chunk.js"
+  },
+  {
+    "revision": "57a04e90eff5ba07dcde",
+    "url": "/pages/static/js/runtime-main.c4d3e3b8.js"
+  },
+  {
+    "revision": "e86c62802179e40f47b1e940c06944f0",
+    "url": "/pages/static/media/logo.e86c6280.png"
+  }
+]);
